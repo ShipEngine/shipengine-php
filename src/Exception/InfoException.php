f764a2e4dbe0396e62c7714944a0f5ce41a74f13
Anthony Shull <shull.anthony@gmail.com>
2020-11-24 17:00:38 -0600

    clean up exceptions

---
+++ src/Exception/InfoException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ShipEngine\Exception;
+
+/**
+ *
+ */
+class InfoException extends ShipEngine\Exception
+{
+}
---
+++ src/Exception/WarningException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ShipEngine\Exception;
+
+/**
+ *
+ */
+class WarningException extends ShipEngine\Exception
+{
+}
