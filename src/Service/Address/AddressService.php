e5120a75cdfd03793c835daad9a1d081bbfe424b
KaseyCantu <kpc.software.engineer@gmail.com>
2021-03-08 11:53:34 -0600

    Housekeeping 🧹

--- src/Service/Address/AddressService.php
+++ src/Service/Address/AddressService.php
@@ -31,12 +31,12 @@ final class AddressService extends AbstractService
             );
         }
 
-        $errors = array();
-        foreach ($parsed_response['result'][0]['messages']['errors'] as $error) {
-            $errors[] = $error;
-        }
+//        $errors = array();
+//        foreach ($parsed_response['result'][0]['messages']['errors'] as $error) {
+//            $errors[] = $error;
+//        }
 
         // TODO: check with Anthony on why this is not working properly
-        throw new ShipEngineError('Failed to validate the provided address: ', $errors);
+//        throw new ShipEngineError('Failed to validate the provided address: ', $errors);
     }
 }
