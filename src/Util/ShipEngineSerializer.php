ab73a997c2baf8f3f1e1f8e33ff430d4ed71a7f1
KaseyCantu <kpc.software.engineer@gmail.com>
2021-03-15 16:50:46 -0500

    Created a class to utilize this Serialization throughout the various services and traits.

--- src/Util/ShipEngineSerializer.php
+++ src/Util/ShipEngineSerializer.php
@@ -2,17 +2,26 @@
 
 namespace ShipEngine\Util;
 
-use ShipEngine\Message\ShipEngineError;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
+/**
+ * Class ShipEngineSerializer
+ * @package ShipEngine\Util
+ */
 final class ShipEngineSerializer
 {
+    /**
+     * @var Serializer
+     */
     private Serializer $serializer;
 
+    /**
+     * ShipEngineSerializer constructor.
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     */
     public function __construct()
     {
         $this->serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
@@ -21,5 +30,16 @@ final class ShipEngineSerializer
     public function deserializeJsonToType(string $json_data, $target_class)
     {
              return $this->serializer->deserialize($json_data, $target_class, 'json');
+    }
+
+    public function serializeDataToType($php_object, $target_class)
+    {
+        $json = $this->serializeDataToJson($php_object);
+        return $this->deserializeJsonToType($json, $target_class);
+    }
+
+    public function serializeDataToJson($data): string
+    {
+        return $this->serializer->serialize($data, 'json');
     }
 }
