a51036ee296855655fbce70e2f3d3adc30469545
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-13 17:21:26 -0500

    Added docstrings and minor updates to be consistent with the rest of the SDK.

--- tests/Message/Events/RequestSentEventTest.php
+++ tests/Message/Events/RequestSentEventTest.php
@@ -1,10 +1,12 @@
 <?php declare(strict_types=1);
 
-namespace ShipEngine\Message\Events;
+namespace Message\Events;
 
 use DateInterval;
 use DateTime;
+use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Psr\Http\Client\ClientExceptionInterface;
 use ShipEngine\Model\Address\Address;
 use ShipEngine\ShipEngine;
 use ShipEngine\Util\Constants\Endpoints;
@@ -15,59 +17,85 @@ use ShipEngine\Util\Constants\RPCMethods;
  * @covers \ShipEngine\Message\Events\ResponseReceivedEvent
  * @covers \ShipEngine\Message\Events\ShipEngineEvent
  * @covers \ShipEngine\Message\Events\ShipEngineEventListener
- * @uses \ShipEngine\Model\Address\Address
- * @uses \ShipEngine\Model\Address\AddressValidateResult
- * @uses \ShipEngine\Service\Address\AddressService
- * @uses \ShipEngine\ShipEngine
- * @uses \ShipEngine\ShipEngineClient
- * @uses \ShipEngine\ShipEngineConfig
- * @uses \ShipEngine\Util\Assert
- * @uses \ShipEngine\Util\VersionInfo
+ * @uses   \ShipEngine\Model\Address\Address
+ * @uses   \ShipEngine\Model\Address\AddressValidateResult
+ * @uses   \ShipEngine\Service\Address\AddressService
+ * @uses   \ShipEngine\ShipEngine
+ * @uses   \ShipEngine\ShipEngineClient
+ * @uses   \ShipEngine\ShipEngineConfig
+ * @uses   \ShipEngine\Util\Assert
+ * @uses   \ShipEngine\Util\VersionInfo
  */
 final class RequestSentEventTest extends MockeryTestCase
 {
-    public function testRequestSent(): void
+    /**
+     * A method using **Mockery Spies** to test the **RequestSentEvent**
+     * being emitted.
+     *
+     * @throws ClientExceptionInterface
+     */
+    public function testRequestSentEvent(): void
     {
-        $spy = \Mockery::spy('ShipEngineEventListener');
-        $configOptions = $this->stubConfig($spy);
-        $ship_engine = new ShipEngine($configOptions);
-        $good_address = $this->stubAddress();
+        $spy = Mockery::spy('ShipEngineEventListener');
+        $config = $this->stubConfig($spy);
+        $shipengine = new ShipEngine($config);
+        $goodAddress = $this->stubAddress();
 
-        $ship_engine->validateAddress($good_address);
+        $shipengine->validateAddress($goodAddress);
 
         $eventResult = null;
         $spy->shouldHaveReceived('onRequestSent')
-            ->withArgs(function ($event) use (&$eventResult) {
-                $eventResult = $event;
-                return true;
-            })
-            ->once();
+            ->withArgs(
+                function ($event) use (&$eventResult) {
+                    $eventResult = $event;
+                    return true;
+                }
+            )->once();
 
-        $this->assertRequestEvent($eventResult, $configOptions);
+        $this->assertRequestEvent($eventResult, $config);
     }
 
-    public function assertRequestEvent($event, $configOptions) : void
+    /**
+     * Tests the assertions outlined in JIRA DX-1550.
+     *
+     * @param RequestSentEvent $event
+     * @param array $config
+     */
+    private function assertRequestEvent(RequestSentEvent $event, array $config): void
     {
         $this->assertInstanceOf(RequestSentEvent::class, $event);
         $this->assertEqualsWithDelta($event->timestamp, new DateTime(), 5);
         $this->assertEquals($event->type, RequestSentEvent::REQUEST_SENT);
-        $this->assertEquals($event->message, $this->expectedMessage($configOptions));
-        $this->assertEquals($event->url, $configOptions['baseUrl']);
-        $this->assertEquals($event->headers['Api-Key'], $configOptions['apiKey']);
+        $this->assertEquals($event->message, $this->expectedMessage($config));
+        $this->assertEquals($event->url, $config['baseUrl']);
+        $this->assertEquals($event->headers['Api-Key'], $config['apiKey']);
         $this->assertEquals($event->headers['Content-Type'], 'application/json');
         $this->assertEquals($event->body['method'], RPCMethods::ADDRESS_VALIDATE);
         $this->assertEquals($event->retry, 0);
-        $this->assertEquals($event->timeout, $configOptions['timeout']);
+        $this->assertEquals($event->timeout, $config['timeout']);
     }
 
-    private function expectedMessage($configOptions) : string
+    /**
+     * A method that returns the expected exception message in
+     * the **testResponseReceivedEvent()** test.
+     *
+     * @param $config
+     * @return string
+     */
+    private function expectedMessage($config): string
     {
-        $url = $configOptions['baseUrl'];
+        $url = $config['baseUrl'];
         $method = RPCMethods::ADDRESS_VALIDATE;
         return "Calling the ShipEngine $method API at $url";
     }
 
-    private function stubAddress() : Address
+    /**
+     * A method that returns a stub address to use in the call to **$shipengine->validateAddress();**
+     * in the **testRequestSentEvent()** test.
+     *
+     * @return Address
+     */
+    private function stubAddress(): Address
     {
         return new Address(
             array(
@@ -80,7 +108,14 @@ final class RequestSentEventTest extends MockeryTestCase
         );
     }
 
-    private function stubConfig($eventListener) : array
+    /**
+     * A method that returns a stub config object to use when instantiating the **ShipEngine** class
+     * in the **testRequestSentEvent()** test.
+     *
+     * @param object $eventListener
+     * @return array
+     */
+    private function stubConfig(object $eventListener): array
     {
         return array(
             'apiKey' => 'baz',
